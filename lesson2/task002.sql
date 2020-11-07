700e58194a224beb61f603f5f276b2026370f52d
FD70000000 <55132316+FD70@users.noreply.github.com>
2020-11-07 20:38:20 +0300

    Update task002.sql

--- lesson2/task002.sql
+++ lesson2/task002.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS example;
 USE example;
 drop table if exists users;
 CREATE TABLE users (
-	id INT UNSIGNED NOT NULL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
 INSERT INTO users VALUES (1, 'user1');
