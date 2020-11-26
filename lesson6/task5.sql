c332bdceced37239d1f2458193ead11535cbc23e
FD70000000 <halfl.ez.ser@mail.ru>
2020-11-27 00:03:04 +0300

    lesson 6, t5

---
+++ lesson6/task5.sql
@@ -0,0 +1,7 @@
+-- 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
+
+-- Которые наименьшую
+SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ LIMIT 10;
+
+-- Которые наибольшую
+SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ DESC LIMIT 10;
