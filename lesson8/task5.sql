ae86e3a2ac1cf7debf6bdce425ce3f0f4ea99b9e
FD70000000 <halfl.ez.ser@mail.ru>
2020-12-04 00:35:42 +0300

    lesson 8, t5

--- lesson8/task5.sql
+++ lesson8/task5.sql
@@ -1,8 +1,10 @@
 
 -- 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
 
--- Которые наименьшую
-SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ LIMIT 10;
+---- Которые наименьшую
+--SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ LIMIT 10;
+--
+---- Которые наибольшую
+--SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ DESC LIMIT 10;
 
--- Которые наибольшую
-SELECT user_id, COUNT(*) AS activ FROM LIKES GROUP BY user_id ORDER BY activ DESC LIMIT 10;
+-- Без понятия, зачем и как тут добавлять JOIN, если одна таблица
