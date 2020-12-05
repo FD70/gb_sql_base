3823bcf2a9a69d9757e9cca3480593336f398fdc
FD70000000 <halfl.ez.ser@mail.ru>
2020-12-05 16:12:07 +0300

    lesson 9, 1t1

---
+++ lesson9/1task1.sql
@@ -0,0 +1,7 @@
+-- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных.
+-- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+
+START TRANSACTION;
+	INSERT INTO sample.users
+    SELECT * FROM hh.users WHERE id = 1;
+COMMIT;
